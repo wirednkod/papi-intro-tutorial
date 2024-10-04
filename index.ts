5a466be06960af50836d3994b0b5934ff17ca7a8
Nikos Kontakis <wirednkod@gmail.com>
2024-10-04 14:40:25 +0300

    solution: makeClient

--- index.ts
+++ index.ts
@@ -1,16 +1,9 @@
-// TODO: Remove the "Hello via Bun!" log.
-console.log("Hello via Bun!");
+import { getWsProvider } from "polkadot-api/ws-provider/web";
+import { createClient, type PolkadotClient } from "polkadot-api";
 
-// TODO:
-// - Import `getWsProvider` from `polkadot-api/ws-provider/web`.
-// - import `createClient` and `type PolkadotClient` from `polkadot-api`;
-
-// TODO:
-// - Create a new function `makeClient`
-//   - Function accepts a parameter `endpoint` with type `string`.
-//   - Function returns the `PolkadotClient` type we imported above.
-// - Write the logic for `makeClient`
-//   - `console.log` a friendly message about which `endpoint` you are connecting to.
-//   - Call `getWsProvider(endpoint)`, and assign the result to a new `const provider`.
-//   - Call `createClient(provider)`, and assign the result to a new `const client`.
-//   - Return `client`.
+function makeClient(endpoint: string): PolkadotClient {
+  console.log(`Connecting to endpoint: ${endpoint}`);
+  const provider = getWsProvider(endpoint);
+  const client = createClient(provider);
+  return client;
+}
